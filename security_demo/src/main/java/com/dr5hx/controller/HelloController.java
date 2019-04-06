4b3b2ade6f95e3e16df014ad732a2a0ecfad8cb8
dr5hx <dr5hx@outlook.com>
2019-04-06 23:59:21 +0800

    初始化版本

---
+++ security_demo/src/main/java/com/dr5hx/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.dr5hx.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("hello")
+    public String HelloService() {
+        return "HelloWorld";
+    }
+}
